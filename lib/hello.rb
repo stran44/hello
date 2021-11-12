343251a887869cdf5d3d6efd6e4a75bde0add62a
Sarah Tran <stran4@worcester.edu>
2021-11-12 18:18:42 -0500

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,8 @@
+require 'greeter'
+
 # Default is "World"
 # Author: Sarah Tran (stran4@worcester.edu)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
